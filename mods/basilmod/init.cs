5e976bf5571265c0de8dceb513f480e9652dd8fc
Mjaed <christophe@roblin.no>
2020-11-29 14:32:55 +0100

    Updated core.zip

--- mods/basilmod/init.cs
+++ mods/basilmod/init.cs
@@ -1,1 +1,24 @@
 exec("basilmodloader/loader.cs");
+
+%xml = new SimXMLDocument() {};
+
+if(%xml.loadFile("data/cm_objects.xml"))
+{
+  %xml.pushFirstChildElement("object_types");
+  echo (%xml.attribute("defaultState"));
+}
+else
+  echo ("not loaded");
+echo(%xml.elementValue());
+%findPattern = "mods/*/cm_objects.xml";
+for( %file = findFirstFile( %findPattern);
+%file !$= "";
+%file = findNextFile( %findPattern ))
+{
+  %localxml = new SimXMLDocument() {};
+  %localxml.load(%file);
+  %localxml.pushFirstChildElement("object_types");
+  %localxml.pushFirstChildElement("object");
+
+  
+}
